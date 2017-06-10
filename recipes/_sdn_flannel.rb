b0c502252b46d7885570ed5cc0db37ee02a33486
Daniel Klopp <daniel.klopp@nio.com>
2017-06-09 22:00:23 -0700

    Flannel updates

--- recipes/_sdn_flannel.rb
+++ recipes/_sdn_flannel.rb
@@ -3,3 +3,17 @@ node['k8s']['nodes'].each do |mac, server|
 	next unless server['master']
 	master_ips << server['ip']['node-port']
 end
+
+template "/etc/kubernetes/kube-flannel.yml" do
+	source "kube-flannel.yml.erb"
+	owner "root"
+	group "root"
+	mode "0644"
+end
+
+template "/etc/kubernetes/kube-flannel-rbac.yml" do
+	source "kube-flannel-rbac.yml.erb"
+	owner "root"
+	group "root"
+	mode "0644"
+end
